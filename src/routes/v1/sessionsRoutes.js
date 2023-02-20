aa683ea442a575560fafc5128047cec3fb3d9f95
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-02-20 02:06:47 +0100

    nueva prueba

--- src/routes/v1/sessionsRoutes.js
+++ src/routes/v1/sessionsRoutes.js
@@ -2,8 +2,8 @@ const express = require("express")
 const router = express.Router()
 const sessionsController = require("../../controllers/sessionsController")
 
-// obtener una sesión
-router.route("/:sessions").get(sessionsController.getOneSession)
+// obtener una sesión y eliminarla
+router.route("/:sessions").get(sessionsController.getOneSession).delete(sessionsController.deleteOneSession)
 
 module.exports.router = router
 
