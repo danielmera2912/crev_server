32e5c5678b5b8a2777251d7c00f95a7d3d59279e
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-02-20 20:24:19 +0100

    cambio

--- src/routes/v1/partidosRoutes.js
+++ src/routes/v1/partidosRoutes.js
@@ -2,12 +2,15 @@ const express = require("express")
 const router = express.Router()
 const partidosController = require("../../controllers/partidosController")
 // las rutas para:
-
-// obtener todos los partidos
-router.route("/").get(partidosController.getAllPartidos).post(partidosController.createOnePartido)
-
-// obtener un partido
-router.route("/:partido").get(partidosController.getOnePartido)
+// se obtendrán todos los partidos y crear un partido
+router.route("/")
+        .get(partidosController.getAllPartidos)
+        .post(partidosController.createOnePartido)
+// Se obtendrá un partido, actualizar un partido y eliminar un partido
+router.route("/:partido")
+     .get(partidosController.getOnePartido)
+     .put(partidosController.updateOnePartido)
+     .delete(partidosController.deleteOnePartido)
 // obtener un partido mediante su ciudad
 router.route("/ciudad/:ciudad").get(partidosController.checkCiudad);
 // obtener un partido mediante su deporte
